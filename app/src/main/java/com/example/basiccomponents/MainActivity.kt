848e9a76aeaf66ec191fbd10338d3c75bcb34b82
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-01-13 13:00:47 +0600

    Pass data between activities

--- app/src/main/java/com/example/basiccomponents/MainActivity.kt
+++ app/src/main/java/com/example/basiccomponents/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.basiccomponents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.RadioButton
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,33 +10,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var count = 0
         btnApply.setOnClickListener{
-            count++
-            var fn = etFirstname.text.toString()
-            var ln = etLastname.text.toString()
-            var cn = etCountry.text.toString()
+            val name = etName.text.toString()
+            val age = etAge.text.toString().toInt()
+            val country = etCountry.text.toString()
 
-            Log.d("Main AActivity", "$fn || $ln  || $cn")
-            clearData()
-            tvCount.text = "$count"
+            Intent(this,SecondActivity::class.java).also {
+                it.putExtra("EXTRA_NAME", name)
+                it.putExtra("EXTRA_AGE", age)
+                it.putExtra("EXTRA_COUNTRY", country)
+                startActivity(it)
+            }
         }
 
-        btnOrder.setOnClickListener{
-            var checkedMeatrbId = rgMeat.checkedRadioButtonId
-            var meat = findViewById<RadioButton>(checkedMeatrbId)
-            var cbone = cb1.isChecked
-            var cbtwo = cb2.isChecked
-            var cbthree = cb3.isChecked
-            var orderstr = "meat ${meat.text} || "+(if(cbone) "cb1" else "") + (if(cbtwo) "cb2" else "") + (if(cbthree) " cb3" else "")
-            tvSummary.text = orderstr
-        }
     }
 
-    fun clearData(){
-        etFirstname.text.clear()
-        etLastname.text.clear()
-        etBirthdate.text.clear()
-        etCountry.text.clear()
-    }
+
 }
---
+++ app/src/main/java/com/example/basiccomponents/SecondActivity.kt
@@ -0,0 +1,22 @@
+package com.example.basiccomponents
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val name = intent.getStringExtra("EXTRA_NAME")
+        val age = intent.getIntExtra("EXTRA_AGE", 0)
+        val country = intent.getStringExtra("EXTRA_COUNTRY")
+
+        val person = "this is $name || $age || $country"
+        tvSec.text = person
+
+    }
+}
