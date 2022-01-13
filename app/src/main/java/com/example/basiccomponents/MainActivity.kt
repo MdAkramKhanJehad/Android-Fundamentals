aeed4974dedc2bc6f1351c50578c07a5f620bbe0
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-01-13 17:08:34 +0600

    menu added

--- app/src/main/java/com/example/basiccomponents/MainActivity.kt
+++ app/src/main/java/com/example/basiccomponents/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.basiccomponents
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +24,24 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.miAddContact -> Toast.makeText(this, "Clicked add contact", Toast.LENGTH_SHORT).show()
+            R.id.miFavourite -> Toast.makeText(this, "Clicked Favourite", Toast.LENGTH_SHORT).show()
+            R.id.miClose -> Toast.makeText(this, "Clicked Close", Toast.LENGTH_SHORT).show()
+            R.id.miSetting -> Toast.makeText(this, "Clicked Setting", Toast.LENGTH_SHORT).show()
+            R.id.miFeedback -> Toast.makeText(this, "Clicked Feedback", Toast.LENGTH_SHORT).show()
+
+        }
+
+        return true
     }
 
 
